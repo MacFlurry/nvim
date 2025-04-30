ba0ac1a0ba9f03efe5c63bbbfe524a261ff951be
omegabk <omegabk@gmail.com>
2025-04-30 09:24:50 +0200

    fix: allow tracking lua/custom for user config (relativenumber enabled)

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,8 @@
+vim.opt.relativenumber = true
+
+---@type ChadrcConfig 
+ local M = {}
+ M.ui = {theme = 'catppuccin'}
+ M.plugins = "custom.plugins"
+ M.mappings = require "custom.mappings"
+ return M
