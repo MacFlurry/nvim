4369f87c8aac6cbc6386bdfc97211a144df044b2
omegabk <omegabk@gmail.com>
2025-04-30 23:37:46 +0200

    fix: load custom lspconfig for LSP Go and others

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,2 +1,10 @@
-local plugins = {}
+local plugins = {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+}
 return plugins
